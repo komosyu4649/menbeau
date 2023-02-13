9d8b825ec5d32bd40341b40fa77525e55c16c0f4
komosyu4649 <komosyu4649@gmail.com>
2023-02-13 10:51:58 +0900

    add: font setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
 import '@styles/index.scss'
+import { Noto_Sans_Javanese, Lexend } from '@next/font/google'
 import { Suspense } from 'react'
 import { Header } from '@components/Header'
 
+const notoSansJa = Noto_Sans_Javanese({
+  variable: '--font-notoSansJa',
+  // subsets: ['latin'],
+  subsets: ['javanese'],
+  weight: ['400', '700'],
+})
+
+const lexend = Lexend({
+  variable: '--font-lexend',
+  subsets: ['latin'],
+  weight: ['600'],
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ja'>
+    <html lang='ja' className={`${notoSansJa.className} ${notoSansJa.variable} ${lexend.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
