5b7112572fc7ab56baad05adfb306476974afad2
komosyu4649 <komosyu4649@gmail.com>
2023-03-26 10:29:32 +0900

    update: Sidebar responsive

--- src/components/SidebarCategory.tsx
+++ src/components/SidebarCategory.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import React, { use, useEffect, useState } from 'react'
 import style from './SidebarCategory.module.scss'
+import { BREAK_POINT } from '@/constants'
 import { useAccordion } from '@/hooks/useAccordion'
 import microCMSCategoryData from 'public/json/microCMSCategoryData.json'
 
@@ -12,23 +13,36 @@ export const SidebarCategory: React.FC = () => {
   // )
 
   const { isOpen, setIsOpen, accordionRef } = useAccordion()
+
   // const [isOpen, setIsOpen] = useState(false)
   // const test = () => {
   //   setIsOpen(!isOpen)
   //   console.log(123, isOpen)
   // }
 
+  const [windowWidth, setWindowWidth] = useState(0)
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+    if (windowWidth > BREAK_POINT) {
+      setIsOpen(true)
+    }
+  }, [windowWidth])
+
   //
   return (
     <nav className={`${style.container} ${isOpen ? style.stateOpen : style.stateClose}`}>
-      <h2
-        className={style.title}
-        onClick={() => setIsOpen(!isOpen)}
-        aria-controls='sidebarCategory'
-        aria-expanded={!isOpen}
-      >
-        Categories
-      </h2>
+      {windowWidth > BREAK_POINT ? (
+        <h2 className={style.title}>Categories</h2>
+      ) : (
+        <button
+          className={style.title}
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls='sidebarCategory'
+          aria-expanded={!isOpen}
+        >
+          Categories
+        </button>
+      )}
       <div className={style.menu} ref={accordionRef} id='sidebarCategory' aria-hidden={!isOpen}>
         <div className={style.menuInner}>
           <ul className={style.menuList}>
--- src/components/SidebarKeyword.tsx
+++ src/components/SidebarKeyword.tsx
@@ -1,24 +1,37 @@
 'use client'
 
-import React, { use } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Keyword } from './Keyword'
 import style from './SidebarKeyword.module.scss'
+import { BREAK_POINT } from '@/constants'
 import { useAccordion } from '@/hooks/useAccordion'
 import microCMSKeywordData from 'public/json/microCMSKeywordData.json'
 
 export const SidebarKeyword: React.FC = () => {
   const { isOpen, setIsOpen, accordionRef } = useAccordion()
 
+  const [windowWidth, setWindowWidth] = useState(0)
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+    if (windowWidth > BREAK_POINT) {
+      setIsOpen(true)
+    }
+  }, [windowWidth])
+
   return (
     <nav className={`${style.container} ${isOpen ? style.stateOpen : style.stateClose}`}>
-      <h2
-        className={style.title}
-        onClick={() => setIsOpen(!isOpen)}
-        aria-controls='sidebarKeyword'
-        aria-expanded={!isOpen}
-      >
-        keywords
-      </h2>
+      {windowWidth > BREAK_POINT ? (
+        <h2 className={style.title}>keywords</h2>
+      ) : (
+        <button
+          className={style.title}
+          onClick={() => setIsOpen(!isOpen)}
+          aria-controls='sidebarCategory'
+          aria-expanded={!isOpen}
+        >
+          keywords
+        </button>
+      )}
       <div className={style.menu} ref={accordionRef} id='sidebarKeyword' aria-hidden={!isOpen}>
         <div className={style.menuInner}>
           <ul className={style.menuList}>
